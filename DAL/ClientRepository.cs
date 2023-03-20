3497ce07be2adc6087ff2dc229b855ad9680726b
ShaniShurkin <shanishurkin@gmail.com>
2023-03-20 21:29:20 +0200

    finish basic functions DAL

--- DAL/ClientRepository.cs
+++ DAL/ClientRepository.cs
@@ -1,5 +1,9 @@
-﻿namespace DAL
+﻿using DAL.Models;
+using System.Security.Cryptography;
+
+namespace DAL
 {
+    //try and catch
     internal class ClientRepository : IClientRepository
 
     {
@@ -11,33 +15,38 @@
            var database = client.GetDatabase(settings.DatabaseName);
            clientsCollection = database.GetCollection<Client>(settings.ClientsCollectionName);
          }
-	 
-        public IMongoCollection<Food> FileService(IDietDatabaseSettings settings)
-        {
-           
-        }
         
-        public async Task<string> AddAsync(Client objectToAdd)
+        public async Task<string> AddAsync(Client client)
         {
-           await 
+            await clientsCollection.InsertOneAsync(client);
+            //return id
+
+            return "";
         }
-        public Task<bool> DeleteAsync(string id)
+        public async Task<bool> DeleteAsync(string code)
         {
-            throw new NotImplementedException();
+            FilterDefinition<Client> filter = Builders<Client>.Filter.Eq("Code", code);
+            await clientsCollection.DeleteOneAsync(filter);
+            //return task
+            return true;
         }
-        public Task<List<Client>> GetAllAsync()
+        public async Task<List<Client>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await clientsCollection.Find(_ => true).ToListAsync();
         }
 
-        public Task<Client> GetSingleAsync(string id)
+        public async Task<Client> GetSingleAsync(string code)
         {
-            throw new NotImplementedException();
+            FilterDefinition<Client> filter = Builders<Client>.Filter.Eq("Code", code);
+            var client = await clientsCollection.Find(filter).FirstOrDefaultAsync();
+            return client;
         }
 
-        public Task<bool> UpdatAsync(Client objectToUpdate)
+        public async Task<bool> UpdatAsync(Client client)
         {
-            throw new NotImplementedException();
+            FilterDefinition<Client> filter = Builders<Client>.Filter.Eq("Code", client.Code);
+            await clientsCollection.ReplaceOneAsync(filter, client);
+            return false;
         }
     }
 }
--- DAL/Models/Client.cs
+++ DAL/Models/Client.cs
@@ -4,51 +4,41 @@
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string _Id { get; set; }
-        //[BsonElement("firstName")]
+        public string Id { get; set; }
+
+        public string Code { get; set; }
+
         public string FirstName { get; set; } = null!;
 
-        //[BsonElement("lastName")]
         public string LastName { get; set; } = null!;
 
-        //[BsonElement("emailAddress")]
         public string EmailAddress { get; set; } = null!;
 
-        //[BsonElement("gender")]
-        public string? Gender { get; set; }
+         public string? Gender { get; set; }
 
         //[BsonElement("bornDate")]
         public DateTime? BornDate { get; set; }
 
-        //[BsonElement("height")]
         public double Height { get; set; }
 
-        //[BsonElement("weight")]
         public double Weight { get; set; }
 
-        //[BsonElement("fitnessLevel")]
         public int FitnessLevel { get; set; }
 
-        //[BsonElement("route")]
         public Route Route { get; set; }
 
-        //[BsonElement("startDate")]
         public DateTime StartDate { get; set; }
 
-        //[BsonElement("duration")]
         public int Duration { get; set; }
 
-        //[BsonElement("progress")]
         public double[] Progress { get; set; }
 
-        //[BsonElement("food")]
         public Dictionary<string, int> Food { get; set; }
 
         public Client(string firstName, string lastName, string emailAddress,
            DateTime bornDate, double height, double weight,
            Route route = Route.health, int fitnessLevel = 50, int duration = 4)
         {
-            //this._Id = ObjectId.GenerateNewId();
             this.FirstName = firstName;
             this.LastName = lastName;
             this.EmailAddress = emailAddress;
